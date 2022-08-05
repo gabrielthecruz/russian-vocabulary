622e6b17795725dfb56e02af8b63d5734af3155e
Gabriel Cruz <gabrielcruz023@users.noreply.github.com>
2022-08-05 02:01:30 -0300

    database created, reading words and plurals done
    plural pages is still in progress.

--- src/Handler/Plural.hs
+++ src/Handler/Plural.hs
@@ -1,12 +1,27 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.Plural where
 
 import Import
 
 getPluralR :: Handler Html
 getPluralR = do
-    defaultLayout [whamlet|<h1 .header>Plural|]
+  ruWords <- runDB $ selectList [RuWordLevel <-. [Just "A1", Just "A2", Nothing], RuWordWordType <-. [Just "noun", Just "adjective"]] []
+  ruForms <- runDB $ selectList [WordFormWordId <-. (map entityKey $ take 150 ruWords), WordFormWordFormType <-. ["ru_noun_pl_nom", "ru_adj_pl_nom"]] []
+  let wordsWithPlural =
+        [ (ruWordAccented ruWord, wordFormForm wordForm, show $ ruWordWordType ruWord)
+          | Entity ruWordKey ruWord <- ruWords,
+            Entity _ wordForm <- ruForms,
+            ruWordKey == wordFormWordId wordForm
+        ]
+      wordsCount = length ruWords
+  defaultLayout $ do
+    setTitle "Plurals - Русский Словарь"
+    $(widgetFile "plural")
