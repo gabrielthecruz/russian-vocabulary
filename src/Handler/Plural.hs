744c0fadc1dcf4fb04d118912c2fc011ab27c096
Gabriel Cruz <gabrielcruz023@users.noreply.github.com>
2022-08-07 17:24:57 -0300

    plural page layout done

--- src/Handler/Plural.hs
+++ src/Handler/Plural.hs
@@ -16,17 +16,20 @@ parseAccented [] = []
 parseAccented (x : '\'' : xs) = (True, x) : parseAccented xs
 parseAccented (x : xs) = (False, x) : parseAccented xs
 
+getRows :: Int -> [a] -> [[a]]
+getRows _ [] = [[]]
+getRows n x = take n x : getRows n (drop n x)
+
 getPluralR :: Handler Html
 getPluralR = do
-  ruWords <- runDB $ selectList [RuWordLevel <-. [Just "A1", Just "A2", Nothing], RuWordWordType <-. [Just "noun"]] []
-  ruForms <- runDB $ selectList [WordFormWordId <-. map entityKey (take 50 ruWords), WordFormWordFormType <-. ["ru_noun_pl_nom", "ru_adj_pl_nom"]] []
+  ruWords <- runDB $ selectList [RuWordWordType <-. [Just "noun"]] []
+  ruForms <- runDB $ selectList [WordFormWordId <-. map entityKey (take 25 ruWords), WordFormWordFormType <-. ["ru_noun_pl_nom", "ru_adj_pl_nom"]] []
   let wordsWithPlural =
         [ (ruWordAccented ruWord, wordFormForm wordForm, toPathPiece ruWordKey)
           | Entity ruWordKey ruWord <- ruWords,
             Entity _ wordForm <- ruForms,
             ruWordKey == wordFormWordId wordForm
         ]
-      wordsCount = length ruWords
   defaultLayout $ do
     setTitle "Plurals - Русский Словарь"
     $(widgetFile "plural")
