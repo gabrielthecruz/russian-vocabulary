5a12f10ef2a880d1dfa0e4ccb378090838ddb446
Gabriel Cruz <gabrielcruz023@users.noreply.github.com>
2022-08-22 11:54:53 -0300

    plurals page layout fixed
    i removed those ugly buttons and added 2 bordered tables. much better now

--- src/Handler/Plural.hs
+++ src/Handler/Plural.hs
@@ -8,11 +8,11 @@
 
 module Handler.Plural where
 
-import Import
-import System.Random ( Random(randomRs), mkStdGen )
-import qualified Prelude
 import Data.List (nub)
+import Import
 import System.CPUTime (getCPUTime)
+import System.Random (Random (randomRs), mkStdGen)
+import qualified Prelude
 
 rndIndex :: Int -> IO [Int]
 rndIndex len = do
@@ -23,8 +23,8 @@ rndIndex len = do
 shuffle :: Int -> [a] -> IO [a]
 shuffle n x = do
   indexes <- liftIO $ rndIndex $ length x
-  return $ take n [x Prelude.!! i | i <- indexes] 
-  
+  return $ take n [x Prelude.!! i | i <- indexes]
+
 parseAccented :: String -> [(Bool, Char)]
 parseAccented [] = []
 parseAccented (x : '\'' : xs) = (True, x) : parseAccented xs
@@ -38,13 +38,15 @@ getPluralR :: Handler Html
 getPluralR = do
   ruWords <- runDB $ selectList [RuWordWordType <-. [Just "noun"]] []
   shuffledWords <- liftIO $ shuffle 100 ruWords
-  ruForms <- runDB $ selectList [WordFormWordId <-. map entityKey shuffledWords, WordFormWordFormType ==. "ru_noun_pl_nom"] [LimitTo 32]
+  ruForms <- runDB $ selectList [WordFormWordId <-. map entityKey shuffledWords, WordFormWordFormType ==. "ru_noun_pl_nom"] [LimitTo 40]
   let wordsWithPlural =
         [ (ruWordAccented ruWord, wordFormForm wordForm, toPathPiece ruWordKey, wordFormBare wordForm)
           | Entity ruWordKey ruWord <- ruWords,
             Entity _ wordForm <- ruForms,
             ruWordKey == wordFormWordId wordForm
         ]
+      pluralWords1 = take 20 wordsWithPlural
+      pluralWords2 = take 20 $ reverse wordsWithPlural
   defaultLayout $ do
     setTitle "Plurals - Русский Словарь"
     $(widgetFile "plural")
